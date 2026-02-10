850b569142e7653cabbaa94b179edd3dcfc8896d
thekhrafid <mdrafidhossen44@gmail.com>
2026-02-10 20:20:11 +0600

    Add: anly html content

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,17 +1,39 @@
 import React from 'react'
 import Container from '../commonComponent/Container'
 function AboutSection() {
-  return (
-    <div>
-        <Container>
-            <div className='grid grid-cols-2'>
-                <div>Grid 1</div>
-                <div>Grid 2</div>
+    return (
+        <div>
+            <Container>
+                <div className='grid grid-cols-2'>
+                    <div>Grid 1</div>
+                    <div>
+                        <div>
+                            <p>About us </p>
+                        </div>
 
-            </div>
-        </Container>
-    </div>
-  )
+                        <div>
+                            <h1>Establishing a community that encourages lifelong learning</h1>
+                        </div>
+
+                        <div>
+                            <p>It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks  The generated Lorem Ipsum is therefore always free from repetitionIt uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks  The generated Lorem Ipsum is therefore always free from repetitionIt uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks  The generated Lorem Ipsum is therefore always free from repetition</p>
+                        </div>
+
+                        <div>
+                            <h2>Our Mission</h2>
+                            <p>It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks  The generated Lorem Ipsum is therefore always free from repetition</p>
+                        </div>
+
+                        <div>
+                            <h2>Our vision</h2>
+                            <p>It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks  The generated Lorem Ipsum is therefore always free from repetition</p>
+                        </div>
+                    </div>
+
+                </div>
+            </Container>
+        </div>
+    )
 }
 
 export default AboutSection
